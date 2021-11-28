94f7c08d27d89a2e27c0721f150cc7122df7ebe7
danypacf <37599736+danypacf@users.noreply.github.com>
2021-11-28 10:13:15 -0500

    Diseños y login

--- app/src/main/java/com/example/jcplicores/MainActivity.kt
+++ app/src/main/java/com/example/jcplicores/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.jcplicores
 
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 import android.os.Bundle
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val log_button = findViewById<Button>(R.id.log_in)
+        log_button.setOnClickListener {
+            val intent = Intent(this, LogIn::class.java)
+            startActivity(intent)
+        }
     }
 }
